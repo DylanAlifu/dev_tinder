1154633deb01e78d052d72d24782516f33f5138f
DylanAlifu <dylann5326@gmail.com>
2025-04-03 19:20:29 -0700

    updated db pass

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://Dylandev:Dodge5326!@dylandev.7d1cg.mongodb.net/dev_tinder"
+    "mongodb+srv://Dylandev:pKNV84KPweqvDVsI@dylandev.7d1cg.mongodb.net/dev_tinder"
   );
 };
 
